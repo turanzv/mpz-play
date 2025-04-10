cf5e7c54242158c7285d57f8475d5b428d990a95
Turan Vural <turanzv@gmail.com>
2025-04-10 16:38:59 -0700

    00 solved

--- 00-connect/src/bob.rs
+++ 00-connect/src/bob.rs
@@ -9,17 +9,13 @@ use serio::{
 async fn main() {
     // Open a connection.
     println!("BOB\tOpening connection with ALICE...");
-
     let stream = tcp_connect(Role::Bob, DEFAULT_LOCAL).await.unwrap();
     let mut channel = Bincode.new_framed(stream);
-
     println!("BOB\tConnection with ALICE established.");
 
     // Wait for Alice to send her number and print it out.
     println!("BOB\tWaiting for Alice's number...");
-    
     let number:u32 = channel.expect_next().await.unwrap();
-
     println!("BOB\tAlice sent: {}", number);
 
     // Increment and send it back.
