0a62037ca44832312486d81ab4f478b01c561d72
Turan Vural <turanzv@gmail.com>
2025-04-10 16:38:37 -0700

    00 solved

--- 00-connect/src/alice.rs
+++ 00-connect/src/alice.rs
@@ -1,8 +1,25 @@
+use common::{tcp_connect, Role, DEFAULT_LOCAL};
+use serio::{
+    codec::{Bincode, Codec},
+    stream::IoStreamExt,
+    SinkExt,
+};
+
 #[tokio::main]
 async fn main() {
     // Open a connection.
+    println!("ALICE\tOpening connection with BOB...");
+    let stream = tcp_connect(Role::Alice, DEFAULT_LOCAL).await.unwrap();
+    let mut channel = Bincode.new_framed(stream);
+    println!("ALICE\tConnection with BOB established.");
+
 
     // Send a number to Bob and wait for Bob's number.
+    println!("ALICE\tSending number to BOB...");
+    channel.send(12u32).await.unwrap();
+    println!("ALICE\tSent 12 to BOB.");    
 
     // Print the number Alice received.
+    let number: u32 = channel.expect_next().await.unwrap();
+    println!("ALICE\tReceived from BOB: {}", number);
 }
--- 00-connect/src/bob.rs
+++ 00-connect/src/bob.rs
@@ -1,8 +1,30 @@
+use common::{tcp_connect, Role, DEFAULT_LOCAL};
+use serio::{
+    codec::{Bincode, Codec},
+    stream::IoStreamExt,
+    SinkExt,
+};
+
 #[tokio::main]
 async fn main() {
     // Open a connection.
+    println!("BOB\tOpening connection with ALICE...");
+
+    let stream = tcp_connect(Role::Bob, DEFAULT_LOCAL).await.unwrap();
+    let mut channel = Bincode.new_framed(stream);
+
+    println!("BOB\tConnection with ALICE established.");
 
     // Wait for Alice to send her number and print it out.
+    println!("BOB\tWaiting for Alice's number...");
+    
+    let number:u32 = channel.expect_next().await.unwrap();
+
+    println!("BOB\tAlice sent: {}", number);
 
     // Increment and send it back.
+    println!("BOB\tSending back incremented number...");
+    channel.send(number + 1).await.unwrap();
+    println!("BOB\tSent back incremented number.");
+
 }
