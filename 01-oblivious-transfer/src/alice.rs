9d32ba2b89656027caafbeaa982c795c2c97a564
Turan Vural <turanzv@gmail.com>
2025-04-10 23:59:13 -0700

    01-ot done

--- 01-oblivious-transfer/src/alice.rs
+++ 01-oblivious-transfer/src/alice.rs
@@ -1,12 +1,35 @@
+use common::{tcp_connect, Role, DEFAULT_LOCAL};
+use serio::codec::{Bincode, Codec};
+use mpz_common::executor::STExecutor;
+use mpz_core::Block;
+use mpz_ot::{
+    chou_orlandi::{Sender, SenderConfig},
+    OTSender, OTSetup,
+};
+
 #[tokio::main]
 async fn main() {
     // Open a connection and create a channel.
+    let stream = tcp_connect(Role::Alice, DEFAULT_LOCAL).await.unwrap();
+    let channel = Bincode.new_framed(stream);
 
     // Create an executor.
-
+    let mut executor = STExecutor::new(channel);
+    
     // Create an OT sender and set it up.
+    let sender_config = SenderConfig::default();
+    let mut sender = Sender::new(sender_config);
+
+    println!("ALICE\tSetting up sender...");
+    sender.setup(&mut executor).await.unwrap();
+    println!("ALICE\tSender setup.");
 
     // Create messages.
+    let zero = Block::ZERO;
+    let one = Block::ONE;
+    println!("ALICE\tFalse: {:?}", zero);
+    println!("ALICE\tTrue:  {:?}", one);
 
     // Send OTs to Bob.
+    sender.send(&mut executor, &[[zero, one]]).await.unwrap();
 }
--- 01-oblivious-transfer/src/bob.rs
+++ 01-oblivious-transfer/src/bob.rs
@@ -1,12 +1,34 @@
+use common::{tcp_connect, Role, DEFAULT_LOCAL};
+use serio::codec::{Bincode, Codec};
+use mpz_common::executor::STExecutor;
+use mpz_ot::{
+    chou_orlandi::{Receiver, ReceiverConfig},
+    OTReceiver, OTSetup,
+};
+
 #[tokio::main]
 async fn main() {
     // Open a connection and create a channel.
-
+    let stream = tcp_connect(Role::Bob, DEFAULT_LOCAL).await.unwrap();
+    let channel = Bincode.new_framed(stream);
+    
     // Create an executor.
+    let mut executor = STExecutor::new(channel);
 
     // Create an OT receiver and set it up.
+    let receiver_config = ReceiverConfig::default();
+    let mut receiver = Receiver::new(receiver_config);
+
+    println!("BOB\tSetting up receiver...");
+    receiver.setup(&mut executor).await.unwrap();
+    println!("BOB\tReceiver setup.");
 
     // Make a choice.
+    let choice = true;
+    println!("BOB\tChoice: {}", choice);
 
     // Receive OTs from Alice.
+    let msg = receiver.receive(&mut executor, &[choice]).await.unwrap();
+    println!("BOB\tReceived from Alice: {:?}", msg.msgs.first().unwrap());
+
 }
